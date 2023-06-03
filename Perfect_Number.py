ff3f56d229d458b48801590eb9dbc1423f9ae629
dedeepya55 <119920060+dedeepya55@users.noreply.github.com>
2023-06-03 18:13:58 +0530

    n=int(input()) s=0 for i in range(1,n):     if n%i==0:         s=s+i if s==n:     print("True") else:     print("False")

--- Perfect_Number.py
+++ Perfect_Number.py
@@ -1,9 +1,9 @@
-a=int(input())
-c=0
-for i in range(1,a):
-    if(a%i==0):
-        c=c+i
-if(c==a):
+n=int(input())
+s=0
+for i in range(1,n):
+    if n%i==0:
+        s=s+i
+if s==n:
     print("True")
 else:
     print("False")
