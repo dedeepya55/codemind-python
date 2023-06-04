47f0bc03971c88fee89b1e0ca5779ad0222b0ae2
dedeepya55 <119920060+dedeepya55@users.noreply.github.com>
2023-06-04 12:40:48 +0530

    n=int(input()) lst=list(map(int,input().split())) a=sum(lst) while(n):     if a%n==0:         print(n)         break     n-=1

--- Maximum_Identical_Bowls.py
+++ Maximum_Identical_Bowls.py
@@ -1,8 +1,8 @@
 n=int(input())
-m=list(map(int,input().split()))
-a=sum(m)
+lst=list(map(int,input().split()))
+a=sum(lst)
 while(n):
-    if(a%n==0):
+    if a%n==0:
         print(n)
         break
     n-=1
